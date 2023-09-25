d0b4323e0f71a59da7d2ea139d04f680afeab718
chaocauminhlason <95207985+chaocauminhlason@users.noreply.github.com>
2023-09-25 14:05:30 +0700

    Create countNode.cpp

---
+++ countNode.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include 'slist.cpp'
+template<class E>
+class baitap:: public Slist<E>{
+public:
+
+int countNode(Node<E> * head)
+if(head==nullptr) return 0;
+return countNode(head-> next);
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
+}
+int main(){
+    baitap<E> a;
+    a.countNode;
+    return 0;
+}
